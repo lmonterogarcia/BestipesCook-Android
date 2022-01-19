22b3b838ea9002fd9450ec76c09dfe71f932e6bc
Luis <Montero>
2022-01-19 10:34:34 +0100

    Creacion de Fagment 2. close #31

--- Bestipescook/app/src/main/java/com/medac/bestipescook/MainActivity.java
+++ Bestipescook/app/src/main/java/com/medac/bestipescook/MainActivity.java
@@ -14,16 +14,19 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.medac.bestipescook.view.ChallengesFragment;
+import com.medac.bestipescook.view.CuentaFragment;
+import com.medac.bestipescook.view.NoticiasFragment;
+import com.medac.bestipescook.view.RankingFragment;
+import com.medac.bestipescook.view.RecetasFragment;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity{
 
-    DrawerLayout drawerLayout;
-
-    NoticiasFragment frNoticias = NoticiasFragment();
-    ChallengesFragment frChallenges = ChallengesFragment();
-    RecetasFragment frRecetas = RecetasFragment();
-    RankingFragment frRanking = RankingFragment();
-    CuentaFragment frCuenta = CuentaFragment();
+    NoticiasFragment frNoticias = new NoticiasFragment();
+    ChallengesFragment frChallenges = new ChallengesFragment();
+    RecetasFragment frRecetas = new RecetasFragment();
+    RankingFragment frRanking = new RankingFragment();
+    CuentaFragment frCuenta = new CuentaFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,36 +34,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigationView = findViewById(R.id.botton_navigation);
-        //navigationView.liste
-
-        drawerLayout = findViewById(R.id.drawerLayout);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar,0,0);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
-        actionBarDrawerToggle.syncState();
-
-        openFrament(new NoticiasFRagment());
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(frNoticias);
     }
 
-    private void openFrament(Fragment fragment){
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.container, fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        drawerLayout.closeDrawer(GravityCompat.START);
-        switch (item.getItemId()){
-            case R.id.menu_noticias:openFrament(new NoticiasFragment()); break;
-            case R.id.menu_challenges:openFrament(new ChallengesFragment()); break;
-            case R.id.menu_recetas:openFrament(new RecetasFragment()); break;
-            case R.id.menu_ranking:openFrament(new RankingFragment()); break;
-            case R.id.menu_cuenta:openFrament(new CuentaFragment()); break;
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_noticias:
+                    loadFragment(frNoticias);
+                    return true;
+                case R.id.menu_challenges:
+                    loadFragment(frChallenges);
+                    return true;
+                case R.id.menu_recetas:
+                    loadFragment(frRecetas);
+                    return true;
+                case R.id.menu_ranking:
+                    loadFragment(frRanking);
+                    return true;
+                case R.id.menu_cuenta:
+                    loadFragment(frCuenta);
+                    return true;
+            }            return false;
         }
-        return false;
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
+
 }
